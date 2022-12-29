41ef60f4a0220c1efccefa4c4ed5e829d524f747
Jared Nutt <jarednut@Jared-Nut-Macbook.local>
2022-12-29 11:43:42 -0800

    fixing lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,10 +2,15 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
+  "cssls",
+  "cssmodules_ls",
   'tsserver',
   'eslint',
   'sumneko_lua',
+  'intelephense',
 })
+
+lsp.nvim_workspace()
 
 lsp.setup()
 
