0f5762d586b932e84720cca75e9da8202bb1d10d
Jared Nutt <jared.nutt@goldenhippo.com>
2023-04-02 17:50:19 -0700

    adding tresitter context

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -43,6 +43,7 @@ return require('packer').startup(function(use)
 
 
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use 'nvim-treesitter/nvim-treesitter-context'
 
   -- LSP ZERO
   use {
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -169,6 +169,11 @@ _G.packer_plugins = {
     path = "/Users/jarednut/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-treesitter-context"] = {
+    loaded = true,
+    path = "/Users/jarednut/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
+  },
   ["nvim-web-devicons"] = {
     loaded = true,
     path = "/Users/jarednut/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
