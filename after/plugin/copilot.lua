fa3cfc767f4d5b1cf6089fdadd4cc252c722a301
Jared Nutt <jared.nutt@goldenhippo.com>
2023-12-14 16:34:42 -0800

    adding copilot config

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,8 @@
+-- vim.cmd[[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
+
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+
+vim.g.copilot_no_tab_map = true
