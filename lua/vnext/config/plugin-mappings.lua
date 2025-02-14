02655b1812bc528d727329cc741022dc5a540548
Jared Nutt <jared.nutt@goldenhippo.com>
2025-02-13 17:33:17 -0800

    moving plugin specific settings to their respective plugin files

--- lua/vnext/config/plugin-mappings.lua
+++ lua/vnext/config/plugin-mappings.lua
@@ -1,16 +1,16 @@
 -- TELESCOPE
 -- TODO: should figure out how to include this in the telescope.lua file
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+-- local builtin = require('telescope.builtin')
+-- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- COPILOT
 -- vim.cmd[[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
-vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', {
-  expr = true,
-  replace_keycodes = false
-})
+-- vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', {
+--   expr = true,
+--   replace_keycodes = false
+-- })
 
 vim.g.copilot_no_tab_map = true
