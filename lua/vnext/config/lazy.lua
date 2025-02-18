20dd8f4136a0cf55fd6eaa3a7c32e9575111808f
Jared Nutt <jared.nutt@goldenhippo.com>
2025-02-18 14:30:12 -0800

    disabling notifications for plugin updates

--- lua/vnext/config/lazy.lua
+++ lua/vnext/config/lazy.lua
@@ -30,8 +30,8 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "catppuccin" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- automatically check for plugin updates but don't tell me
+  { enabled = true, notify = false }
 })
 
 
